3e4fd3c957b8981cdee480532788717bbd118ab5
fuyukaidesu <hue@pomf.fr>
2014-10-15 20:32:40 +0200

    Reecriture documentation logger

--- include/Log.hpp
+++ include/Log.hpp
@@ -3,13 +3,28 @@
 #include <string>
 
 /**
- * \class Logger Cette classe gere les information a afficher, selon le niveau parametré
+ * \class Logger
+ * Gère les informations à afficher, selon le niveau parametré
  */
 struct Logger
 {
+	/**
+	 *\var filter
+	 * Niveau de sortie autorisé, les demandes de log avec un niveau inférieur
+	 * à cette variable ne seront pas reportés
+	 */
+	/**
+	 *\var fd
+	 * Descripteur du fichier dans lequel écrire les logs.
+	 */
 	int filter, fd;
+	/**
+	 *\param Filter Niveau de filtre
+	 *\param Fd Descripteur du fichier dans lequel écrire les logs.
+	 */
 	Logger(int Filter = 0, int Fd = 2) : filter(Filter), fd(Fd) {}
 
+	/// Cette énumeration contient les différents niveau d'urgence existant
 	enum
 	{
 		Debug,
@@ -19,6 +34,11 @@ struct Logger
 		Error,
 		Critical,
 	};
+
+	/**
+	 *\param rhs Chaine à écrire
+	 *\param lvl Niveau d'urgence
+	 */
 	void operator()(const std::string& rhs = std::string(), int lvl = 1) const
 	{
 		if(lvl < filter) return;
