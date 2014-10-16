054c04537ce901363f8f7c0137e1919ebdd731a5
fuyukaidesu <hue@pomf.fr>
2014-10-16 05:59:06 +0200

    Support Manette XBox de Microsoft

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -15,11 +15,16 @@ shared_ptr<Controller> Controller::getController(float sensitivity)
 {
 	for(int i = 0; i < 16; ++i)
 		if(glfwJoystickPresent(i))
-			if(string("Generic X-Box pad") == glfwGetJoystickName(i))
+		{
+			if(string("Generic X-Box pad") == glfwGetJoystickName(i) ||
+			   string("Microsoft X-Box 360 pad") == glfwGetJoystickName(i))
 			{
 				Application::Log("Found an XBox Controller", Logger::Info);
 				return make_shared<XBoxController>(i, sensitivity);
 			}
+			else
+				Application::Log(string("Found a joypad : ") + glfwGetJoystickName(i));
+		}
 	Application::Log("Couldn't find a suitable controller", Logger::Debug);
 	return make_shared<DummyController>();
 }
